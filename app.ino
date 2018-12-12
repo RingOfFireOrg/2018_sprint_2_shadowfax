3fcfc94e6555d37e587569880252e1f3a1c4656b
unknown <foo@foo.com>
2018-12-11 20:05:54 -0500

    initial commit  Changes to be committed: 	modified:   app.ino

--- app.ino
+++ app.ino
@@ -5,10 +5,21 @@ Servo servoRight;
 Servo servoArm;
 Servo servoRaise;
 
-const int kPinServoLeft = 7;
-const int kPinServoRight = 2;
-const int kPinServoArm = 3;
-const int kPinServoRaise = 4;
+const int kPinServoLeft = 8;
+const int kPinServoRight = 9;
+const int kPinServoRaise = 10;
+const int kPinServoArm = 11;
+
+// pin 8 is left wheel 
+// pin 9 is right wheel
+// pin 10 is winch
+// pin 11 is arm
+
+buttonPressed = LOW;
+buttonNotPressed = HIGH;
+
+bool grabberClosed = false;
+bool grabberRaised = true;
 
 void setup()
 {
@@ -20,11 +31,59 @@ void setup()
 
 void loop()
 {
-
+    //input: WASD, f = hinge and g = grabber
     //if(input*){
     //}
-  servoLeft.write(180);
+ /* servoLeft.write(180);
         delay(2000);
         servoLeft.write(0);
         delay(2000);
+*/
+
+/*
+if(digitalRead(kPinServoArm) == buttonPressed){
+
+    if grabberClosed = false{
+
+        grabberClosed = true;
+
+        servoArm.write(180);
+
+    }
+
+
+    else if grabberClosed = true{
+
+        grabberClosed = false;
+
+            servoArm.write(0);
+
+    }
+}
+
+if(digitalRead(kPinServoRaise) == buttonPressed){
+
+    if grabberRaised = false{
+
+        grabberRaised = true;
+
+        servoRaise.write(180);
+
+    }
+
+
+    else if grabberRaised = true{
+
+        grabberRaised = false;
+
+            servoRaise.write(0);
+
+    }
+}
+*/
+
+
+
+
+
 }
